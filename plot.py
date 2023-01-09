f867268dd3457f530842f3a2bf46b67ebe83fb1c
SimonB00 <simone.balducci@gmail.com>
2023-01-09 20:02:41 +0100

    New data points for plot and added fit line

--- plot.py
+++ plot.py
@@ -1,12 +1,24 @@
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
+# Define the data
 years = [1977,1987,1998,2006,2010,2013,2014,2015,2019,2021]
-areas = [1,0.808214600448317,0.581066614619375,0.304787167976721,0.264261253748292,0.271470986591908,0.155804751681187,0.211492396955233,0.168222170963525,0.159078408062599]
+areas = [1,0.7733,0.5273,0.2169,0.2058,0.1854,0.0904,0.1423,0.0859,0.0821]
+years = np.array(years).reshape((-1,1))
 
+# Fit the data
+model = LinearRegression()
+model.fit(years, areas)
+q = model.intercept_
+m = model.coef_
+
+# Plot
 plt.plot(years, areas)
 plt.plot(years, areas, 'bo')
+plt.plot(years, [-0.02216*x + 44.7826 for x in years])
 plt.xticks(fontsize=15)
 plt.yticks(fontsize=15)
-plt.xlabel("Years", fontsize=15)
-plt.ylabel("Normalize surface area", fontsize=15)
-plt.show()
+plt.xlabel('Years', fontsize=15)
+plt.ylabel('Normalize surface area', fontsize=15)
+plt.savefig('./tex/linear_plot.pgf')
